afd0d0a76d0a0058d1dd32f4527d7226b2c07ae8
Pool Lau <rpalm160@outlook.com>
2023-07-08 19:43:05 -0500

    Adición del proyecto Web MiAlmacen

--- MiAlmacen.AdminLayer/App_Start/BundleConfig.cs
+++ MiAlmacen.AdminLayer/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace MiAlmacen.AdminLayer
 {
@@ -8,16 +7,20 @@ namespace MiAlmacen.AdminLayer
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new Bundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new Bundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new Bundle("~/bundles/complementos").Include(
-                        "~/Scripts/scripts.js",
-                        "~/Scripts/fontawesome/all.min.js"));
+                "~/Scripts/scripts.js",
+                "~/Scripts/DataTables/jquery.dataTables.js",
+                "~/Scripts/DataTables/dataTables.responsive.js",
+                "~/Scripts/fontawesome/all.min.js"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.bundle.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/DataTables/css/jquery.dataTables.css",
+                "~/Content/DataTables/css/responsive.dataTables.css", 
+                "~/Content/site.css"));
         }
     }
 }
--- MiAlmacen.AdminLayer/Controllers/EstadoController.cs
+++ MiAlmacen.AdminLayer/Controllers/EstadoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
